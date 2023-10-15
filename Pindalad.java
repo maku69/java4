cac9dcf1b5c9aac87d30824fabc3fc3694bd915d
maku69 <81355250+maku69@users.noreply.github.com>
2023-10-15 17:24:50 +0300

    Add files via upload

---
+++ Pindalad.java
@@ -0,0 +1,9 @@
+public class Pindalad {
+    public static double ristkylikuPindala(double pikkus, double laius) {
+        return pikkus * laius;
+    }
+
+    public static double ringiPindala(double raadius) {
+        return Math.PI * raadius * raadius;
+    }
+}
